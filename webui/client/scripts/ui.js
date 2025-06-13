b4b989c8c993416e45470fbe452e7f2e686d88b4
ia74 <68617740+ia74@users.noreply.github.com>
2025-06-13 16:57:26 -0500

    change size of freedeck logo

--- webui/client/scripts/ui.js
+++ webui/client/scripts/ui.js
@@ -83,7 +83,7 @@ function makeBootLog() {
   const thisbootLog = document.createElement("div");
   thisbootLog.id = "boot-log-div";
   thisbootLog.innerHTML =
-    "<img src='/assets/logo_big.png' class='n-icon'><h1>Freedeck</h1><div style='display:none;' id='boot-log'><center class='oclb flex-wrap-r'><button id='oclb'>Close Boot Log</button></center></div>";
+    "<img src='/assets/logo_big.png' class='n-icon'><h1 style='font-weight:500;'>Freedeck</h1><div style='display:none;' id='boot-log'><center class='oclb flex-wrap-r'><button id='oclb'>Close Boot Log</button></center></div>";
   document.body.appendChild(thisbootLog);
   bootLog = thisbootLog;
   bootLogCenter = document.querySelector("#boot-log-div > center");
