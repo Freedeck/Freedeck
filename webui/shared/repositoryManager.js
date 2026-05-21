2b19aa6d0ed4a5f9ff6743799b0562326213c116
ia74 <aidenjs9@icloud.com>
2026-05-21 10:57:33 -0500

    - marketplace: finally remove legacy text-style repository, remove all support for it - marketplace: add better styling for the preset/url repo adder

--- webui/shared/repositoryManager.js
+++ webui/shared/repositoryManager.js
@@ -8,11 +8,10 @@ const repom = {
 		},
 	],
 	unofficial: [],
-	getLegacyRepository,
-	getV3Repository,
+	getRepository,
 };
 
-async function getV3Repository(repoData) {
+async function getRepository(repoData) {
 	const plugins = [];
 	const res = await fetch(`${repoData.url}?time=${Date.now()}`).catch((err) => {
 		return err;
@@ -37,40 +36,6 @@ async function getV3Repository(repoData) {
 	}
 
 	return { ...channel, plugins };
-}
-
-async function getLegacyRepository(url) {
-	return new Promise((_resolve, reject) => {
-		const _plugins = [];
-		const res = fetch(url)
-			.catch((err) => {
-				reject(err);
-			})
-			.then((res) => {
-				return res.text();
-			})
-			.then((data) => {
-				if (!data.includes(",!"))
-					reject({ err: true, msg: "No plugin metadata found." });
-				let lines = data.split("\n");
-				lines.shift();
-				lines = lines.filter((line) => line.length > 0);
-				for (const line of lines) {
-					const comma = line.split(",!");
-					const meta = {
-						file: comma[0],
-						githubRepo: `https://github.com/${comma[1]}`,
-						name: comma[2],
-						author: comma[3],
-						version: comma[4],
-						description: comma[5],
-						id: comma[6],
-					};
-					_plugins.push(meta);
-				}
-				_resolve(_plugins);
-			});
-	});
 }
 
 export default repom;
