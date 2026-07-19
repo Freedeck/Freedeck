8f5913f687ade0907412ce10a272e911697d4b31
ia74 <aidenjs9@icloud.com>
2026-07-19 12:18:27 -0500

    - single lock for overlay

--- src/app/overlay/launcher.js
+++ src/app/overlay/launcher.js
@@ -1,6 +1,11 @@
 const { app } = require("electron");
 const makeWindow = require("../makeWindow");
 const launcherObject = require("./window");
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit();
+}
 app.on("ready", () => {
 	makeWindow(launcherObject);
 });
