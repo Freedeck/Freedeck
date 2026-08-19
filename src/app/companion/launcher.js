b8f6da62602b7e4683c42e044e9f83c6f2f49613
ia.74 <aidenjs9@icloud.com>
2026-08-18 23:30:32 -0500

    - try to make people want to use new method and migrate them

--- src/app/companion/launcher.js
+++ src/app/companion/launcher.js
@@ -6,7 +6,26 @@ const launcherObject = require("./window");
 const net = require("net");
 const os = require("os");
 const path = require("node:path");
+const { writeFileSync, existsSync } = require("node:fs");
 
+const pipePath = '\\\\.\\pipe\\fd_app_handoff';
+
+const client = net.createConnection({ path: pipePath }, () => {
+	if(!existsSync('download.new.freedeck.launcher')) writeFileSync('download.new.freedeck.launcher', 0)
+  dialog.showErrorBox("Old Launcher Detected!", `Freedeck has automatically detected that you are still using the outdated "App" (launcher/updater).
+
+Freedeck has completely consolidated into one app, removing the requirement of pressing Launch every time you open the app.
+
+Please download the latest version on GitHub (repository: freedeck/freedeck), or Freedeck.app!
+
+Freedeck will automagically move your data over for you, when you make the switch.
+
+This configuration has been marked for update.
+
+Thank you for using Freedeck.`)
+  client.end();
+});
+client.on('error', (err) => {  })
 app.on("ready", () => {
 	makeWindow(launcherObject);
 
