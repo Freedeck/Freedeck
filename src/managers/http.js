7e0154a04d03b520ff91070231a5e599f9970b4e
ia.74 <aidenjs9@icloud.com>
2026-09-01 20:59:21 -0500

    - Reduce amount of dependencies loaded by each HTTP handler

--- src/managers/http.js
+++ src/managers/http.js
@@ -1,31 +1,26 @@
-const bonjour = require("bonjour-service");
 const express = require("express");
-const path = require("node:path");
+const http = require("node:http");
 
-const picocolors = require("$/picocolors");
 const { recordTime } = require("$/timer");
 const debug = require('$/debug');
-const os = require("node:os");
 
-const bonjourInstance = new bonjour();
+recordTime("managers/http:loaded");
+
 const config = require("@managers/settings");
 const settings = config.settings();
-debug.log("Loaded settings.", "Server / HTTP")
+debug.log("Loaded settings.", "Server / HTTP");
 
-const http = require("node:http");
 const { getStartupMessage } = require("./startupMessage");
 const app = express();
 const server = http.createServer(app);
 
-const pkgLoc = path.resolve("package.json");
-const thisPackage = require(pkgLoc);
-
 const PORT = settings.port || 5754;
 
 let preInitActive = true;
+let bonjourInstance = null;
 
-app.get('/api/discover', (req,res, next) => {
-  if(!preInitActive) {next();return;}
+app.get('/api/discover', (req, res, next) => {
+  if (!preInitActive) { next(); return; }
   res.send({
     title: 'Freedeck',
     version: 'Loading...',
@@ -33,53 +28,67 @@ app.get('/api/discover', (req,res, next) => {
     startupMessage: getStartupMessage(),
     ready: false,
     ip: {},
-    myApp:{code:'',host:'null'}
-  })
-})
+    myApp: { code: '', host: 'null' }
+  });
+});
 
 module.exports = {
-  http, server, app, 
+  http, server, app,
   deactivatePrerun: () => {
     preInitActive = false;
   }
-}
+};
 
 recordTime("http:listen-begin");
-debug.log("Beginning listen task", "Server / HTTP")
+debug.log("Beginning listen task", "Server / HTTP");
+
 server.listen(PORT, "0.0.0.0", () => {
-	debug.log("FD is now listening.", "Server / HTTP")
-	const networkAddresses = require("@managers/networkAddresses");
-	const netAddresses = networkAddresses();
-	
-	const localHostName = os.hostname().replace(/\.local\.?$/i, "");
+  debug.log("FD is now listening.", "Server / HTTP");
 
-	bonjourInstance.publish({
-		name: `Freedeck (${localHostName})`,
-		type: "freedeck",
-		port: Number(PORT),
-		txt: {
-			version: String(thisPackage.version),
-			hostname: localHostName,
-			addresses: JSON.stringify(netAddresses),
-		},
-	});
+	recordTime("http:bonjour-start")
+  const os = require("node:os");
+  const path = require("node:path");
+  const picocolors = require("$/picocolors");
+  const Bonjour = require("bonjour-service");
+  const networkAddresses = require("@managers/networkAddresses");
 
-	console.log(
-		picocolors.bgGreen(`Bonjour advertising '_freedeck._tcp' on port ${PORT}`),
-	);
+  const thisPackage = require(path.resolve("package.json"));
+  const netAddresses = networkAddresses();
+  const localHostName = os.hostname().replace(/\.local\.?$/i, "");
 
-	for (const netInterface of Object.keys(netAddresses)) {
-		const ipPort = `${netAddresses[netInterface][0]}:${PORT}`;
-		console.log(
-			picocolors.bgBlue(
-				`Go to http://${ipPort} on your mobile device (${netInterface})`,
-			),
-		);
-	}
-	recordTime("http:listen-complete");
+	recordTime("http:bonjour-created")
+  bonjourInstance = new Bonjour();
+  bonjourInstance.publish({
+    name: `Freedeck (${localHostName})`,
+    type: "freedeck",
+    port: Number(PORT),
+    txt: {
+      version: String(thisPackage.version),
+      hostname: localHostName,
+      addresses: JSON.stringify(netAddresses),
+    },
+  });
+	recordTime("http:bonjour-complete")
+
+  console.log(
+    picocolors.bgGreen(`Bonjour advertising '_freedeck._tcp' on port ${PORT}`),
+  );
+
+  for (const netInterface of Object.keys(netAddresses)) {
+    const ipPort = `${netAddresses[netInterface][0]}:${PORT}`;
+    console.log(
+      picocolors.bgBlue(
+        `Go to http://${ipPort} on your mobile device (${netInterface})`,
+      ),
+    );
+  }
+
+  recordTime("http:listen-complete");
 });
 
 process.on("SIGINT", () => {
-	bonjourInstance.destroy();
-	process.exit(0);
+  if (bonjourInstance) {
+    bonjourInstance.destroy();
+  }
+  process.exit(0);
 });
